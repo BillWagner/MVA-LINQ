328a0dc63dd004cac4f00df3aeb9bb61ee55359d
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-17 21:58:05 -0400

    Provide an implementation of Where
    This is local, so it is a better match than the Linq.Enumerable methods

--- CodePlayground/Program.cs
+++ CodePlayground/Program.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace CodePlayground
 {
+    public static class MyLinqMethods
+    {
+        public static IEnumerable<T> Where<T>(
+            this IEnumerable<T> inputSequence, 
+            Func<T, bool> predicate)
+        {
+            foreach (T item in inputSequence)
+                if (predicate(item))
+                    yield return item;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             // Generate items using a factory:
             var items = GenerateSequence(i => i.ToString());    
-            foreach (var item in items)
+            foreach (var item in items.Where(item => item.Length < 2))
                 Console.WriteLine(item);
 
+            return;
             var moreItems = GenerateSequence(i => i);
             foreach (var item in moreItems)
                 Console.WriteLine(item);
