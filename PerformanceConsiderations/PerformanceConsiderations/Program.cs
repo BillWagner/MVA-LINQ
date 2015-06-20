3ac1e6890dac89155604660624c0307f1265d88d
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-20 11:31:28 -0400

    Demo to show lazy composition

--- PerformanceConsiderations/PerformanceConsiderations/Program.cs
+++ PerformanceConsiderations/PerformanceConsiderations/Program.cs
@@ -10,10 +10,24 @@ namespace PerformanceConsiderations
     {
         static void Main(string[] args)
         {
-            var sequence = Enumerable.Range(0, int.MaxValue);
+
+            var sequence = from n in Enumerable.Range(0, 20)
+                           where n % 3 == 0
+                           select n * n;
+
+            var sequence2 = from item in sequence
+                            let label = item.ToString()
+                            orderby label descending
+                            select new { label, item };
+
+            foreach (var item in sequence2)
+                Console.WriteLine(item);
+
+            return;
+            var sequence3 = Enumerable.Range(0, int.MaxValue);
 
             Console.WriteLine("Starting");
-            int singleItem = sequence.Last(item => item == 13);
+            int singleItem = sequence3.Last(item => item == 13);
             Console.WriteLine(singleItem);
         }
     }
