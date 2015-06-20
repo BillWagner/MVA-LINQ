6690690f3be97417a2a1c0f719bcc6148bc52e4e
Bill Wagner <bill.w.wagner@outlook.com>
2015-06-20 08:27:02 -0400

    Query Expression Pattern, Listing 3
    Select with a projection

--- QueryExpressionPattern/QueryExpressionPattern/Program.cs
+++ QueryExpressionPattern/QueryExpressionPattern/Program.cs
@@ -12,7 +12,8 @@ namespace QueryExpressionPattern
         {
             int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             var squares = from n in numbers
-                          select n * n;
+                          select new { Number = n, Square = n * n };
+            var squares2 = numbers.Select(n => new { Number = n, Square = n * n });
 
         }
     }
